ffcd67f312fcfc838983241496eeb365e03107eb
junwoo.lee <junwoo.lee@navercorp.com>
2021-01-30 01:57:41 +0900

    first commit

---
+++ src/main/kotlin/com/study/kolearn/KolearnApplication.kt
@@ -0,0 +1,11 @@
+package com.study.kolearn
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KolearnApplication
+
+fun main(args: Array<String>) {
+    runApplication<KolearnApplication>(*args)
+}
---
+++ src/test/kotlin/com/study/kolearn/KolearnApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.kolearn
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KolearnApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
