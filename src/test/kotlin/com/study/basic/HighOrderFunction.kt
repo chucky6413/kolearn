9aceede2db0e0116bf787e9f195078bddbdd70fb
junwoo.lee <junwoo.lee@navercorp.com>
2021-02-14 17:20:48 +0900

    add highOrderFunc

--- src/test/kotlin/com/study/basic/HighOrderFunction.kt
+++ src/test/kotlin/com/study/basic/HighOrderFunction.kt
@@ -1,7 +1,38 @@
 package com.study.basic
 
+import org.junit.jupiter.api.Test
+
 class HighOrderFunction {
+    @Test
+    fun sum() {
+        val sum = { x: Int, y: Int -> x + y }
+        println(sum(1, 3))
+
+        val sum2: (Int, Int) -> Int = { x, y -> x + y }
+        println(sum2(2, 4))
+    }
+
+    @Test
+    fun anonymousFunc() {
+        fun calculator(a: Int, b: Int, p: (Int, Int) -> Int) {
+            println("$a, $b -> ${p(a, b)}")
+        }
+        calculator(2, 1) { x, y -> x + y }
 
 
+        fun highOrderFuncPrint(a: String, p: (String) -> String) {
+            println("${p(a)}")
+        }
+        highOrderFuncPrint("hello") { it }
 
+        fun highOrderFuncPrint2(p: () -> Unit) {
+            p()
+        }
+        highOrderFuncPrint2 { println("hi~") }
+
+        fun printInfo(p: (() -> Unit)? = null) {
+            p?.invoke() ?: println("not exist p()")
+        }
+        printInfo()
+    }
 }
